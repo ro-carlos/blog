cd51407d9acb6ad7dce26c8e081383f7803857ec
Carlos Andrés Rodríguez Torres <andresrodriguez@worldofficeonline.com>
2020-06-16 09:57:04 -0500

    COMMIT: Navbar menu position

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+paddingBottom = 0;
+checked = false;
+
+function onClickNavButton(element) {
+  checked = !checked;
+  const nav = document.getElementById("nav-menu");
+  const navLinks = document.getElementById("nav-links");
+
+  if (checked) {
+    // nav.style.paddingBottom = "calc(100vh - 50px)";
+    nav.style.paddingBottom = "290px";
+  } else if (!checked) {
+    nav.style.paddingBottom = "0px";
+  }
+}
